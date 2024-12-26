eaa6a9bfe7fd441c891eb2745cb40a4cc1596e5e
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2024-12-26 22:41:50 +0330

    growth update.

--- src/growth.rs
+++ src/growth.rs
@@ -19,13 +19,31 @@ fn create_and_save_canvas_of_size<const H: usize, const W: usize>() {
     px.default_image_builder().save("arts/growth.png").unwrap();
 }
 
+fn create_and_save_maybe_canvas_of_size<const H: usize, const W: usize>() {
+    let mut px = PixelCanvas::<H, W, MaybePixel>::default();
+
+    px.draw(CENTER, square::<1>(BLACK));
+    px.default_image_builder()
+        .save("arts/maybe_growth.png")
+        .unwrap();
+}
+
 #[test]
 fn test_growth() {
     // That's enough ?!
-    // A single 110x110 canvas will cause the stack to overflow!
-    // currently ~11,881 normal [`Pixel`]s can be handled at most.
-    // This can surely be further increased, but is it needed?
-    create_and_save_canvas_of_size::<109, 109>();
+    // A single 313x313 normal canvas (not transparent) will cause the stack to overflow!
+    // currently ~97,000 normal [`Pixel`]s can be handled at most.
+    // Pretty enough for something that should look pixely, But many things IDK.
+    create_and_save_canvas_of_size::<312, 312>();
+}
+
+#[test]
+fn test_maybe_growth() {
+    // That's enough ?!
+    // A single 271x271 maybe_canvas will cause the stack to overflow!
+    // currently ~73,000 [`MaybePixel`]s can be handled at most.
+    // Pretty enough for something that should look pixely, But many things IDK.
+    create_and_save_maybe_canvas_of_size::<270, 270>();
 }
 
 #[test]
